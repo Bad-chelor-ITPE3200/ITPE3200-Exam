c5fc3e70464f5d2e0110c8d9fc123ede8e5783b9
Oliver Dragland <oliver.dragland@gmail.com>
2023-09-11 13:16:43 +0200

    Add project files.

---
+++ FastFlat/Controllers/RentalController.cs
@@ -0,0 +1,36 @@
+﻿using FastFlat.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastFlat.Controllers
+{
+    public class RentalController : Controller
+    {
+        public IActionResult RentalCards()
+        {
+            var rentals = new List<Rental>();
+            var rental1 = new Rental
+            {
+                rentalId = 1,
+                name = "Fin leilighet i Oslo sentrum",
+                address = "Osloveien 21B",
+                price = 1800,
+                fromDate = new DateOnly(2023, 9, 15),
+                toDate = new DateOnly(2023, 9, 18),
+            };
+            var rental2 = new Rental
+            {
+                rentalId = 2,
+                name = "Stor familievilla i sentrum",
+                address = "Solveien 23, Trondheim",
+                price = 2750,
+                fromDate = new DateOnly(2023, 9, 18),
+                toDate = new DateOnly(2023, 9, 26),
+            };
+            rentals.Add(rental1);
+            rentals.Add(rental2);
+
+            ViewBag.CurrentViewName = "List of rentals.";
+            return View(rentals);
+        }
+    }
+}
---
+++ FastFlat/Models/Rental.cs
@@ -0,0 +1,14 @@
+﻿namespace FastFlat.Models
+{
+    public class Rental
+    {
+        public int rentalId { get; set; }
+        public string name { get; set; } = string.Empty;
+        public string? description { get; set; }
+        public string address { get; set; } = string.Empty;
+        public double price { get; set; }
+        public DateOnly fromDate { get; set; }
+        public DateOnly toDate { get; set; }
+        public string? imageURL { get; set; }
+    }
+}
