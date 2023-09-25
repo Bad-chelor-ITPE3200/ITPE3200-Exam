6b84ee9450c0966869c4ce31d1fb025f341036cf
Oliver Dragland <oliver.dragland@gmail.com>
2023-09-25 15:56:33 +0200

    PUML diagrams

--- FastFlat/ViewModels/RentalListViewModel.cs
+++ FastFlat/ViewModels/RentalListViewModel.cs
@@ -7,11 +7,13 @@ namespace FastFlat.ViewModels
     public class RentalListViewModel
     {
         public IEnumerable<ListningModel> Rentals;
+        public IEnumerable<BookingModel> Bookings;
         public string? CurrentViewName;
 
-        public RentalListViewModel(IEnumerable<ListningModel> rentals, string? currentViewName) {
+        public RentalListViewModel(IEnumerable<ListningModel> rentals, string? currentViewName, IEnumerable<BookingModel> bookings) {
             Rentals = rentals;
             CurrentViewName = currentViewName;
+            Bookings = bookings; 
         }
     }
 }
