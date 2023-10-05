02e81eb346cf09c2254bd0b9f6e48af115819f75
Ali Imran Anjum <89576402+Aliimrananjum@users.noreply.github.com>
2023-10-05 09:57:14 +0200

    Added new nav "accounts" to test out Authoriz feature.

---
+++ FastFlat/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FastFlat.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [Authorize]
+        public async Task<IActionResult> Account()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+        }
+    }
+}
