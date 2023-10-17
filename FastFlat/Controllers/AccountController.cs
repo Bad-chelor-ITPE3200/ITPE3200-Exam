9b0e7cb2d06ca549dd3237039c71bc8bf6f5cd47
jpwiig <jpwiig@gmail.com>
2023-10-17 16:28:28 +0200

    created a new version of without partial veiw on admin account, cleaning up after a while

--- FastFlat/Controllers/AccountController.cs
+++ FastFlat/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 
 namespace FastFlat.Controllers
 {
@@ -13,11 +14,13 @@ namespace FastFlat.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IRentalRepository<BookingModel> _bookingrepository;
-        public AccountController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IRentalRepository<BookingModel>bookingrepository)
+        private readonly IRentalRepository<ApplicationUser> _ApplicationUserRepostiory; 
+        public AccountController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IRentalRepository<BookingModel>bookingrepository, IRentalRepository<ApplicationUser> applicationUserRepostiory)
         {
             _userManager = userManager;
             _roleManager = roleManager;
-            _bookingrepository = bookingrepository; 
+            _bookingrepository = bookingrepository;
+            _ApplicationUserRepostiory = applicationUserRepostiory; 
         }
 
         [Authorize]
@@ -33,14 +36,14 @@ namespace FastFlat.Controllers
         public async Task<IActionResult> ManageAllBookings()
         {
             //return View(AdminPage); 
-            var bookings = _bookingrepository.GetAll();
+            var bookings = _bookingrepository.GetAll().ToList();
             return View(bookings);
         }
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> _AdminAccounts()
         {
-            var users = _userManager.Users.ToListAsync();
-            return View();
+            var users = _userManager.Users;
+            return View(users);
         }
         [HttpGet]
         [Authorize(Roles = "Admin")]
@@ -48,8 +51,24 @@ namespace FastFlat.Controllers
         {
           await _bookingrepository.Delete(id);
           return RedirectToAction(nameof(ManageAllBookings)); 
-            
+          
         }
-        
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteAdminAccount(string userId)
+        {
+            var user = _userManager.FindByIdAsync(userId).Result;
+            await _userManager.DeleteAsync(user);
+            return RedirectToAction(nameof(_AdminAccounts)); 
+        }
+
+       [HttpPost]
+       [Authorize(Roles = "Admin")]
+       public async Task<IActionResult> UpdateAdminAccount(ApplicationUser userupdated)
+       {
+        await _userManager.UpdateAsync(userupdated);
+        return RedirectToAction(nameof(_AdminAccounts)); 
+       }
     }
 }
