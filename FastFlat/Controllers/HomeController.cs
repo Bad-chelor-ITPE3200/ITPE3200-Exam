328d9486296e60b0f64ae9d276537154e3386a6a
Oliver Dragland <oliver.dragland@gmail.com>
2023-10-24 15:28:44 +0200

    Added dynamic slider to the index page.

--- FastFlat/Controllers/HomeController.cs
+++ FastFlat/Controllers/HomeController.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FastFlat.DAL;
+using FastFlat.Models;
+using FastFlat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FastFlat.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IRentalRepository<ListningModel> _rentalRepo;
+        private readonly IRentalRepository<AmenityModel> _amenityRepo;
+
+        public HomeController(IRentalRepository<ListningModel> rentalRepo, IRentalRepository<AmenityModel> amenityRepo)
         {
-            return View();
+            _rentalRepo = rentalRepo;
+            _amenityRepo = amenityRepo;
         }
 
-
+        public IActionResult Index()
+        {
+            var rentalList = _rentalRepo.GetAll();
+            var amenityList = _amenityRepo.GetAll();
+            var rentalListViewModel = new RentalListViewModel(rentalList.Take(5), amenityList, "Card");
+            return View(rentalListViewModel);
+        }
     }
 }
